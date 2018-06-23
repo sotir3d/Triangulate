e82909d4e9e9d9cf03633adf250645f0a46008a3
SEBI-PC\Sebastian <sebastian.fueloep@gmail.com>
2018-06-23 23:17:45 +0200

    First Commit

---
+++ Triangulate/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveSpeed = 20;
+
+    Rigidbody2D rigidBody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
+
+    }
+}
